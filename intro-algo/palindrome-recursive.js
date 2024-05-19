fc9988a6a939e93a5cfaf8177193e97dc1fe5607
Thanasak1412 <Thanasak.sr60@snru.ac.th>
2024-05-19 11:11:19 +0700

    Add palindrome function with recursive

---
+++ intro-algo/palindrome-recursive.js
@@ -0,0 +1,11 @@
+function isPalindrome(str = "") {
+  if (str.length <= 1) {
+    return true;
+  }
+
+  if (str[0] !== str[str.length - 1]) {
+    return false;
+  }
+
+  return isPalindrome(str.slice(1).slice(str.length));
+}

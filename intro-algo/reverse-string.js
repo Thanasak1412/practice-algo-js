55b0baaaf32d27a06bbddeed49d3cb84224f74ff
Thanasak1412 <Thanasak.sr60@snru.ac.th>
2024-05-26 12:59:31 +0700

    Reverse string

---
+++ intro-algo/reverse-string.js
@@ -0,0 +1,12 @@
+function reverseString(string = "") {
+  if (!string) {
+    return "";
+  }
+
+  let reversedString = "";
+  for (let i = string.length - 1; i >= 0; i--) {
+    reversedString += string[i];
+  }
+
+  return reversedString;
+}

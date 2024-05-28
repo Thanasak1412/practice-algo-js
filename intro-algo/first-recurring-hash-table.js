a5339266855e96991668c8886447aed997354306
Thanasak1412 <Thanasak.sr60@snru.ac.th>
2024-05-28 22:09:19 +0700

    First recurring with hash table

--- intro-algo/first-recurring-hash-table.js
+++ intro-algo/first-recurring-hash-table.js
@@ -1,6 +1,14 @@
 const arr = [1, 2, 3, 5, 4, 5, 6, 2, 4];
 
-function findDuplicate(arr) {
+function firstRecurring(arr) {
+  if (!arr.length) {
+    return undefined;
+  }
+
+  if (arr.length === 1) {
+    return arr;
+  }
+
   const lookup = {};
 
   for (let item of arr) {

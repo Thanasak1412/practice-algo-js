ecd8338c819dc817b36b4ebb8d318ff501f883fd
Thanasak1412 <Thanasak.sr60@snru.ac.th>
2024-05-30 13:32:12 +0700

    fix: fix dequeue

--- intro-algo/queue.js
+++ intro-algo/queue.js
@@ -12,6 +12,7 @@ class Queue {
     this.length = 0;
   }
 
+  // * Add an element to the end of the queue
   enqueue(value) {
     const newNode = new Node(value);
 
@@ -28,6 +29,7 @@ class Queue {
     this.length++;
   }
 
+  // * Remove the element from the front of the queue
   dequeue() {
     if (this.isEmpty()) {
       return null;
@@ -35,6 +37,7 @@ class Queue {
 
     const { value } = this.head;
 
+    this.head = this.head.next;
     if (!this.head) {
       this.tail = null;
     }
@@ -44,8 +47,23 @@ class Queue {
     return value;
   }
 
+  // * Retrieve the element an the front of the queue without removing it
+  front() {
+    if (this.isEmpty()) {
+      return undefined;
+    }
+
+    return this.head.value;
+  }
+
+  // * Check if the queue is empty
   isEmpty() {
-    return !this.head;
+    return !this.length;
+  }
+
+  // * Get number of elements in the queue
+  getSize() {
+    return this.length;
   }
 }
 
@@ -53,4 +71,8 @@ const q = new Queue();
 
 q.enqueue(10);
 q.enqueue(11);
+q.enqueue(12);
 q.dequeue();
+q.dequeue();
+console.log(q.front());
+console.log(q.getSize());
